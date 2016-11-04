fc8b64b822c0cef7c1c0ed2891d96057eaf9783e
jackal998 <jackal998@gmail.com>
2016-11-04 17:10:24 +0800

    Init_Datebase

---
+++ db/migrate/20161104085656_add_name_serial_date_date_r_value_sn1_sn2_des_user_to_property.rb
@@ -0,0 +1,13 @@
+class AddNameSerialDateDateRValueSn1Sn2DesUserToProperty < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :objs, :name, :string
+  	add_column :objs, :serial, :string
+  	add_column :objs, :datebought, :date
+  	add_column :objs, :dateretire, :date
+  	add_column :objs, :value, :integer
+  	add_column :objs, :snumber1, :string
+  	add_column :objs, :snumber2, :string
+  	add_column :objs, :description, :text
+  	add_column :objs, :user, :string
+  end
+end
