18328329150a76a35518e9b5e41da3834ffba259
jackal998 <jackal998@gmail.com>
2016-11-05 16:26:13 +0800

    destroy_same_page

--- app/controllers/objs_controller.rb
+++ app/controllers/objs_controller.rb
@@ -1,11 +1,45 @@
 class ObjsController < ApplicationController
+
+
+
 	def index
 		@objs = Obj.page(params[:page]).per(10)
+		@@pgv = params[:page]
 	end
 	def new
-
+		@obj = Obj.new
 	end
 	def show
 		@obj = Obj.find(params[:id])
+	end
+	def edit
+		@obj = Obj.find(params[:id])
+	end
+	def update
+		@obj = Obj.find(params[:id])
+		@obj.update(obj_params)
+		redirect_to obj_path(@obj)
+	end
+	def destroy
+		@obj = Obj.find(params[:id])
+		
+		@obj.destroy
+
+		redirect_to (objs_path+"?page=#{@@pgv}")
+	end
+
+	private
+
+	def obj_params
+		params.require(:obj).permit(
+			:name, 
+			:serial, 
+			:value, 
+			:snumber1, 
+			:snumber2, 
+			:string,
+			:description, 
+			:text,
+			:user)
 	end
 end
