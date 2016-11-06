c04f0ae46bb1285e5955ad041a417a176f95651c
jackal998 <jackal998@gmail.com>
2016-11-06 18:30:24 +0800

    validate_information_pos_fixed

--- app/models/obj.rb
+++ app/models/obj.rb
@@ -1,8 +1,8 @@
 class Obj < ApplicationRecord
 	validates_presence_of :name
 	validates_uniqueness_of :serial
-	validates :datebought, format: { with: /\d{4}-\d{2}-\d{2}/, message: "datebought: wrong format" }
-	validates :dateretire, format: { with: /\d{4}-\d{2}-\d{2}/, message: "dateretire: wrong format" }
+	validates :datebought, format: { with: /\d{4}-\d{2}-\d{2}/, message: "format should be: yyyy-mm-dd" }
+	validates :dateretire, format: { with: /\d{4}-\d{2}-\d{2}/, message: "format should be: yyyy-mm-dd" }
 	validates_numericality_of :value, :only_integer => true # 必須是整數
 	validates_presence_of :description
 end
