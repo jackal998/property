765302d24f5df437ed41ed095af98e8f2cd3fdaf
jackal998 <jackal998@gmail.com>
2016-11-07 15:59:30 +0800

    db_add_obj

--- db/migrate/20161104085656_add_name_serial_date_date_r_value_sn1_sn2_des_user_to_property.rb
+++ db/migrate/20161104085656_add_name_serial_date_date_r_value_sn1_sn2_des_user_to_property.rb
@@ -1,7 +1,7 @@
 class AddNameSerialDateDateRValueSn1Sn2DesUserToProperty < ActiveRecord::Migration[5.0]
   def change
-    create_table :events do |t|
-      t.timestamps
+    create_table :objs do |o|
+      o.timestamps
     end
 
   	add_column :objs, :name, :string
