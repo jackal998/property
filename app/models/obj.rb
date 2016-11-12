f47bb236f683eda50bb179add1d13b577fdea44b
jackal998 <jackal998@gmail.com>
2016-11-12 12:24:54 +0800

    log_in form_finished

--- app/models/obj.rb
+++ app/models/obj.rb
@@ -5,4 +5,6 @@ class Obj < ApplicationRecord
 	validates :dateretire, format: { with: /\A(\d(([02468][048])|([13579][26]))\-((((0[13578])|(1[02]))\-((0[1-9])|([1-2][0-9])|(3[01])))|(((0[469])|(11))\-((0[1-9])|([1-2][0-9])|(30)))|(02\-((0[1-9])|([1-2][0-9])))))|(\d(([02468][1235679])|([13579][01345789]))\-((((0[13578])|(1[02]))\-((0[1-9])|([1-2][0-9])|(3[01])))|(((0[469])|(11))\-((0[1-9])|([1-2][0-9])|(30)))|(02\-((0[1-9])|(1[0-9])|(2[0-8])))))\z/, message: "format should be: yyyy-mm-dd" }
 	validates_numericality_of :value, :only_integer => true # 必須是整數
 	validates_presence_of :description
+
+  belongs_to :user
 end
