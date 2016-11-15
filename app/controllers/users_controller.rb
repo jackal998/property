a30572c5fde703b44458f936002c492492b8a69c
jackal998 <jackal998@gmail.com>
2016-11-16 00:33:54 +0800

    profile_objs_show_upgrade'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user)
+    @user_comments = @user.comments.includes(:obj)
     @user_objs = @user.objs.includes(:comments)
   end
 end
